56b5d58a7ce32fd8e1ffa71b7c7abcac4018f098
Moses <mosesdilts@gmail.com>
2024-03-01 12:26:48 -0500

    it almost works

--- hw04.c
+++ hw04.c
@@ -1,32 +1,85 @@
 #include <stdio.h>
 
-void calcLetter(short, char*, char*);
+void calcLetter(int, int*, char*, char*);
+int getYear();
+void printData(int, int, char, char);
 
 int main()
 {
     char YDL1;
     char YDL2;
     int year;
+    int day;
 
-    printf("Enter year -> ");
-    scanf("%d", &year);
-
-    calcLetter(year, &YDL1, &YDL2);
-
-    printf("YDL1: %c YDL2: %c", YDL1, YDL2);
+    getYear(&year);
+    calcLetter(year, &day, &YDL1, &YDL2);
+    printData(year, day, YDL1, YDL2);
 
     return 0;
 }
 
-
-void calcLetter(int year, char* YDL1, char* YDL2)
+int getYear(int *year)
 {
-    int day;
-
-    day = ((year - 1) * 365 + ((year - 1 ) / 4) - ((year - 1) / 100) + ((year - 1) / 400)) % 7; //Initial calculation
-    day = (day + 1) % 7; //Move from dec 31 to jan 01
-    YDL1 = 71 - day; //Convert to char
-
-    year % 4 ? *YDL1 > 'G' ? *YDL2 = YDL1 - 1 : *YDL2 = 'A' : *YDL2 = ' '; //Calculate second YDL if needed
+    printf("Enter year -> ");
+    scanf("%d", year);
 }
 
+void calcLetter(int year, int *day, char *YDL1, char *YDL2)
+{
+    *day = ((year - 1) * 365 + ((year - 1 ) / 4) - ((year - 1) / 100) + ((year - 1) / 400)) % 7; //Initial calculation
+    *day = (*day + 1) % 7; //Move from dec 31 to jan 01
+
+    if(year % 4 == 0) //if leap year
+    {
+        *YDL1 = (char)71 - *day; //Convert to correct char
+        *YDL2 = *YDL1;
+        (*YDL1 != 'G') ? (*YDL1 += 1) : (*YDL1 = 'A');
+    }
+    else //if not leap year
+    {
+        *YDL1 = (char)65 + *day; //Convert to correct char
+        *YDL2 = ' '; //no second YDL needed if leap year
+    }
+}
+
+void printData(int year, int day, char YDL1, char YDL2){
+
+    switch(day) //print out the correct day of the week
+    {
+        case 1:
+            printf("01 January %d falls on a: Monday", year);
+        break;
+        case 2:
+            printf("01 January %d falls on a: Tuesday", year);
+        break;
+        case 3:
+            printf("01 January %d falls on a: Wednesday", year);
+        break;
+        case 4:
+            printf("01 January %d falls on a: Thursday", year);
+        break;
+        case 5:
+            printf("01 January %d falls on a: Friday", year);
+        break;
+        case 6:
+            printf("01 January %d falls on a: Saturday", year);
+        break;
+        case 7:
+            printf("01 January %d falls on a: Sunday", year);
+        break;
+    }
+
+    (YDL2 != ' ') ? printf("\nDominical leters:           %c%c\n", YDL1, YDL2) : printf("\nDominical leters:           %c\n", YDL1);
+
+    if(YDL1 == 'A' && YDL2 == ' ')
+    {
+        printf("Friday the 13th falls in:   January, October");
+    }
+    else if(YDL1 == 'B' && YDL2 == 'A')
+    {
+        printf("Friday the 13th falls in:   October");
+    }
+           
+
+
+}

34970b09325e0350fc490972c99832ed16941ce4
TidyMoMan <77814251+TidyMoMan@users.noreply.github.com>
2024-03-12 11:43:45 -0400

    updated stuff

--- hw05.c
+++ hw05.c
@@ -27,28 +27,55 @@ int getRandInRange(int min, int max)
 
 void getSMM(int *seed, int *min, int *max) //(get seed, min, max)
 {
-    printf("Enter the desired seed value -> ");
-    scanf("%d", seed);
+    *seed = -1;
+    *min = -1;
+    *max = -1;
 
-    printf("Enter the desired minimum value -> ");
-    scanf("%d", min);
+    while(*seed < 0)
+    {
+        printf("Enter the desired seed value -> ");
+        scanf("%d", seed);
 
-    printf("Enter the desired maximum value -> ");
-    scanf("%d", max);
+        if(*seed < 0)
+        {
+            printf("\nError! Minimum must be a non-negative integer.\n");
+        }
+    }
+
+    while(*min < 0)
+    {
+        printf("Enter the desired minimum value -> ");
+        scanf("%d", min);
+
+        if(*min < 0)
+        {
+            printf("\nError! Minimum must be a non-negative integer.\n");
+        }
+    }
+
+    while(*max < *min)
+    {
+        printf("Enter the desired maximum value -> ");
+        scanf("%d", max);
+
+        if(*max < *min)
+        {
+            printf("\nError! Maximum must be greater than %d\n", *min);
+        }
+    }
 }
 
 void calcNumbers(int seed, int randomNum)
 {
     printf("Larger numbers generated from %d: ", seed);
-    printf("%d", getNumDigits(seed));
-
-    int i = 0;
-    while(i < getNumDigits(seed))
-    {
-        i++;
-        printf("%d", getNthDigit(seed, i));
-    }
-
+    int toMult = 0;
+    int digits = 0;
+    printf("original: %d", randomNum);
+    toMult = randomNum % getNumDigits(randomNum);
+    digits = getNumDigits(randomNum);
+    randomNum = (randomNum - (randomNum % 10))/10;
+    randomNum += toMult * (digits * 10);
+    printf("\ngenerated: %d", randomNum);
 }
 
 int getNthDigit(int input, int n)

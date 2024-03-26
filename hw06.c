385787de079fdcf9ccb205f8a53bef2c5fdb6db9
TidyMoMan <77814251+TidyMoMan@users.noreply.github.com>
2024-03-26 15:24:40 -0400

    gotta go

--- hw06.c
+++ hw06.c
@@ -11,20 +11,23 @@ int main(){
 
     toBin(input);
 
-
     return 0;
 }
 
 long long toBin(long long input){
-    int i;
-    int binary[32];
+    int i; //lcv
+    int binary[32]; //binary number as an array
     input > -1 ? binary[32] = 0 : binary [32] = 1;
-
-    for(i = 0; i < 23; i++){
+    for(i = 0; i < 31; i++){
         binary[i] = (input / (int)pow(2, i)) % 2;
         printf("%d", binary[i]);
     }
+    return binary;
+}
 
-    
-    
+int[] toFloat(int binary[]){
+    int values[]; //used to return sign, exponent, and mantissa
+
+
+    return []
 }

32587b73bd8e7bee02d5913b0abfbf3d168ae499
TidyMoMan <77814251+TidyMoMan@users.noreply.github.com>
2024-04-12 10:34:44 -0400

    hw7 worrk

--- hw07.c
+++ hw07.c
@@ -1,39 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PRINT printf("hello world") 
 
 void getSMM(int*, int*, int*);
+void findNumbers(int, int, int*, int*);
 
 int main(){
     int seed; //seed for random generator
     int min; //minimum allowed number 
     int max; //maximum allowed number
 
-    int numbers[];
-    int occur[];
+    int numbers[1000];
+    int occur[100];
 
     getSMM(&seed, &min, &max);
     srand(seed);
-    findNumbers(min, max, numbers[], occur[])
+    findNumbers(min, max, numbers, occur);
 
-
+    PRINT;
 
     return 0;
 }
 
-void findNumbers(int min, int max, int *numbers[], int *occur[]){
-    int i; //lcv
-    int j; //lcv2
-    for(i = 0; i < max; i++){
-        numbers[i] = rand() % (max + 1 - min) + min;
-    }
+void findNumbers(int min, int max, int numbers[], int occur[]){
+    // int i; //lcv
+    // int j; //lcv2
+    // int maxnum = 0; //counter
+    // int digits = 0; //digit counter
 
-    while(){
-        for(i = 0; i <  1000; i++){ //for every number in the array
-            for(j = 0; j < 100; j++){ //count each digit
-                
-            }
-        }
-    }
+    // for(i = 0; i < max; i++){ //fill with random numbers
+    //     numbers[i] = rand() % (max + 1 - min) + min;
+    // }
+
+    // while(true){
+    //     for(i = 0; i <  1000; i++){ //for every number in the array
+    //         for(j = 0; j < numbers[i] / 10 + 1; j++){ //count each digit
+    //             occur[numbers[j] % 10]++;
+    //             numbers[j] /= 10;
+    //             digits++;
+    //         }
+
+    //         for(j = j; j > 0; j--){ //check if there is any digit that makes up more than half of the digit
+    //             *occur[j] > maxnum ? maxnum = *occur[j] : 0;     
+    //         }
+    //         if(max > digits/2){
+    //             printf("true");
+    //         }
+
+    //     }
+    // }
     
 }
 
